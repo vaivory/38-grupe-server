6c39732d5ebc526015819735894c18ad140f3c44
vaivory <100865446+vaivory@users.noreply.github.com>
2022-06-21 20:07:11 +0300

    is valid fullname tests

--- public/js/components/IsValid.js
+++ public/js/components/IsValid.js
@@ -3,14 +3,53 @@
 class IsValid {
     //cia turesim kruva metodu kurie galis patikrinti ar varads geras, ir tt
     static fullname(str) {  //statiniai metodai nereikalauja objekto, galima tiesiog i juos kreiptis IsValid.fullname
-        if (str.length<2)  {
-            return [true,'Per trumpas fullname tesktas'];
+        if (str === undefined) {
+            return [true, 'Neduotas parametras'];
         }
 
-        const parts=str.split('');
-        if (parts.length<2){
-            return [true,'Per mazai fullname zodziu'];
+       // const parts=str.split('');
+       if (typeof str !== 'string') {
+        return [true, 'Netinkamas tipas, turi buti "string"'];
+    }
+    str = str.trim().replace(/\s+/g, ' ');
+        // str = str.trim().replaceAll('  ', ' '); // nes pas mane sitas neveikia :(
+
+        const minWordsCount = 2;
+        const minWordLength = 2;
+        const minTextLength = minWordsCount * minWordLength + (minWordsCount - 1);
+        if (str.length < minTextLength) {
+            return [true, `Per trumpas tekstas, turi buti minimum ${minTextLength} simboliai`];
         }
+
+        const allowedSymbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const words = str.split(' ');
+        if (words.length < minWordsCount) {
+            return [true, `Tekstas turi tureti ${minWordsCount} arba daigiau zodziu`];
+        }
+        for (const word of words) {
+            if (word.length < minWordLength) {
+                return [true, `Visos vardo dalys turi buti minimum ${minWordLength} simboliu`];
+            }
+
+            // pirma raide
+            if (word[0].toUpperCase() !== word[0]) {
+                return [true, `Pirma zodzio raide turi buti didzioji`];
+            }
+
+            // kitos raides
+            const otherLetters = word.slice(1);
+            if (otherLetters.toLowerCase() !== otherLetters) {
+                return [true, `Ne pirma zodzio raide turi buti mazoji`];
+            }
+
+            // ar tik leistinos raides
+            for (const s of word) {
+                if (!allowedSymbols.includes(s)) {
+                    return [true, `Neleistinas simbolis "${s}"`];
+                }
+            }
+        }
+
         return [false, 'OK'];
 
     }
--- public/js/components/IsValid.test.js
+++ public/js/components/IsValid.test.js
@@ -1,9 +1,183 @@
 import { IsValid } from "./IsValid.js";
 
-describe('gaudome netinkamus tipus', ()=> {
-    test('err: number', () => {
-        const [err, msg]=IsValid.fullname(1);
-        expect (err).toBe(true);
-        expect(msg).toBe('Turi buti string tipas');
+describe('Gaudome netinkamus tipus', () => {
+    test('no params', () => {
+        const [err, msg] = IsValid.fullname();
+        expect(err).toBe(true);
+        expect(msg).toBe('Neduotas parametras');
     })
-});
+
+    test('number', () => {
+        const [err, msg] = IsValid.fullname(1);
+        expect(err).toBe(true);
+        expect(msg).toBe('Netinkamas tipas, turi buti "string"');
+    })
+
+    test('boolean', () => {
+        const [err, msg] = IsValid.fullname(true);
+        expect(err).toBe(true);
+        expect(msg).toBe('Netinkamas tipas, turi buti "string"');
+    })
+
+    test('array', () => {
+        const [err, msg] = IsValid.fullname([]);
+        expect(err).toBe(true);
+        expect(msg).toBe('Netinkamas tipas, turi buti "string"');
+    })
+
+    test('null', () => {
+        const [err, msg] = IsValid.fullname(null);
+        expect(err).toBe(true);
+        expect(msg).toBe('Netinkamas tipas, turi buti "string"');
+    })
+
+    test('object', () => {
+        const [err, msg] = IsValid.fullname({});
+        expect(err).toBe(true);
+        expect(msg).toBe('Netinkamas tipas, turi buti "string"');
+    })
+
+    test('function', () => {
+        const [err, msg] = IsValid.fullname(() => { });
+        expect(err).toBe(true);
+        expect(msg).toBe('Netinkamas tipas, turi buti "string"');
+    })
+})
+
+describe('Gauname netinkamas reiksmes', () => {
+    test('empty string', () => {
+        const [err, msg] = IsValid.fullname('');
+        expect(err).toBe(true);
+        expect(msg).toBe('Per trumpas tekstas, turi buti minimum 5 simboliai');
+    })
+
+    test('single symbol too short', () => {
+        const [err, msg] = IsValid.fullname('Petr');
+        expect(err).toBe(true);
+        expect(msg).toBe('Per trumpas tekstas, turi buti minimum 5 simboliai');
+    })
+
+    test('single word', () => {
+        const [err, msg] = IsValid.fullname('Petra');
+        expect(err).toBe(true);
+        expect(msg).toBe('Tekstas turi tureti 2 arba daigiau zodziu');
+    })
+
+    test('long single word', () => {
+        const [err, msg] = IsValid.fullname('PetrasPetraitis');
+        expect(err).toBe(true);
+        expect(msg).toBe('Tekstas turi tureti 2 arba daigiau zodziu');
+    })
+
+    test('one of words is too short', () => {
+        const [err, msg] = IsValid.fullname('Petras P');
+        expect(err).toBe(true);
+        expect(msg).toBe('Visos vardo dalys turi buti minimum 2 simboliu');
+    })
+
+    test('few words is too short', () => {
+        const [err, msg] = IsValid.fullname('Petras P A Petraitis');
+        expect(err).toBe(true);
+        expect(msg).toBe('Visos vardo dalys turi buti minimum 2 simboliu');
+    })
+
+    test('not allowed symbol: 🎅', () => {
+        const [err, msg] = IsValid.fullname('Santa 🎅🎅🎅 Clause');
+        expect(err).toBe(true);
+        expect(msg).toBe('Neleistinas simbolis "🎅"');
+    })
+
+    test('not allowed symbol: 5', () => {
+        const [err, msg] = IsValid.fullname('Santa Area5 Clause');
+        expect(err).toBe(true);
+        expect(msg).toBe('Neleistinas simbolis "5"');
+    })
+
+    test('not allowed symbol: .', () => {
+        const [err, msg] = IsValid.fullname('Santa Area. Clause');
+        expect(err).toBe(true);
+        expect(msg).toBe('Neleistinas simbolis "."');
+    })
+
+    test('uppercase letter: end', () => {
+        const [err, msg] = IsValid.fullname('Santa MariA');
+        expect(err).toBe(true);
+        expect(msg).toBe('Ne pirma zodzio raide turi buti mazoji');
+    })
+
+    test('uppercase letter: middle', () => {
+        const [err, msg] = IsValid.fullname('SanTa Maria');
+        expect(err).toBe(true);
+        expect(msg).toBe('Ne pirma zodzio raide turi buti mazoji');
+    })
+
+    test('uppercase letter: all', () => {
+        const [err, msg] = IsValid.fullname('SANTA MARIA');
+        expect(err).toBe(true);
+        expect(msg).toBe('Ne pirma zodzio raide turi buti mazoji');
+    })
+
+    test('lowercase letter: first (1)', () => {
+        const [err, msg] = IsValid.fullname('santa Maria');
+        expect(err).toBe(true);
+        expect(msg).toBe('Pirma zodzio raide turi buti didzioji');
+    })
+
+    test('lowercase letter: first (2)', () => {
+        const [err, msg] = IsValid.fullname('Santa maria');
+        expect(err).toBe(true);
+        expect(msg).toBe('Pirma zodzio raide turi buti didzioji');
+    })
+
+    test('mixed upper/lower letters (1)', () => {
+        const [err, msg] = IsValid.fullname('SanTa marIa');
+        expect(err).toBe(true);
+        expect(msg).toBe('Ne pirma zodzio raide turi buti mazoji');
+    })
+
+    test('mixed upper/lower letters (2)', () => {
+        const [err, msg] = IsValid.fullname('sANTA mARIA');
+        expect(err).toBe(true);
+        expect(msg).toBe('Pirma zodzio raide turi buti didzioji');
+    })
+})
+
+describe('Gauname tinkamas reiksmes, bet su atleistinomis klaidomis', () => {
+    test('many spaces in between', () => {
+        const [err, msg] = IsValid.fullname('Jonas   Jonaitis');
+        expect(err).toBe(false);
+        expect(msg).toBe('OK');
+    })
+
+    test('many spaces in front', () => {
+        const [err, msg] = IsValid.fullname('   Jonas Jonaitis');
+        expect(err).toBe(false);
+        expect(msg).toBe('OK');
+    })
+
+    test('many spaces at the end', () => {
+        const [err, msg] = IsValid.fullname('Jonas Jonaitis   ');
+        expect(err).toBe(false);
+        expect(msg).toBe('OK');
+    })
+})
+
+describe('Gauname tinkamas reiksmes', () => {
+    test('two words', () => {
+        const [err, msg] = IsValid.fullname('Petras Petraitis');
+        expect(err).toBe(false);
+        expect(msg).toBe('OK');
+    })
+
+    test('three words', () => {
+        const [err, msg] = IsValid.fullname('Jon Bon Jovi');
+        expect(err).toBe(false);
+        expect(msg).toBe('OK');
+    })
+
+    test('four words', () => {
+        const [err, msg] = IsValid.fullname('Jean Clode Van Damme');
+        expect(err).toBe(false);
+        expect(msg).toBe('OK');
+    })
+})
